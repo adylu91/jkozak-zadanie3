314ce9257e6520f08ee6547ac07263c026b63e3d
Adam Dylus <a.adamdylus@gmail.com>
2022-04-16 16:51:58 +0200

    pozostałe metody do zrobienia: usunPowtorzenia i zapiszDoPliku

--- src/pl/adamdylus/Lista.java
+++ src/pl/adamdylus/Lista.java
@@ -1,5 +1,9 @@
 package pl.adamdylus;
 
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+
 public class Lista {
     private int[] liczby;
     private int pojemnosc;
@@ -11,15 +15,63 @@ public class Lista {
         this.liczby = new int[this.pojemnosc];
     }
 
-    public int[] getLiczby() {
-        return liczby;
+    public void dodajElement(int liczbaDoDodania){
+        if(rozmiar > (pojemnosc-1)){ //czy tablica jest pełna
+            System.out.println("Error! Array is full");
+        }else {
+            liczby[rozmiar] = liczbaDoDodania;
+            rozmiar++;
+        }
     }
 
-    public int getPojemnosc() {
-        return pojemnosc;
+    public int znajdz(int szukanaLiczna){
+        for(int i=0; i<liczby.length; i++){
+            if(liczby[i] == szukanaLiczna)
+                return i;
+        }
+        return -1;
     }
 
-    public int getRozmiar() {
-        return rozmiar;
+    public void pisz(){
+        StringBuilder elementy = new StringBuilder();
+        for(int i=0; i<rozmiar; i++){
+            elementy.append(liczby[i]).append(" ");
+        }
+        System.out.println("Lista: ");
+        System.out.println("    Pojemonosc: " + pojemnosc);
+        System.out.println("    Rozmiar: " + rozmiar);
+        System.out.println("    Elementy: " + elementy);
+    }
+
+    public void usunPierwszy(int liczba){
+        boolean czyZnaleziono = false;
+        int indexLiczbyDoUsuniecia = 0;
+        int[] arr = new int[pojemnosc];
+
+        for(int i=0; i<rozmiar; i++){
+            if(liczby[i] == liczba){
+                indexLiczbyDoUsuniecia = liczba-1;
+                czyZnaleziono = true;
+                break;
+            };
+        }
+
+        if(!czyZnaleziono){
+            System.out.println("Nie znaleziono elementu do usuniecia");
+        }else{
+            int j = 0;
+            for(int i = 0; i < rozmiar; i++){
+                if(indexLiczbyDoUsuniecia != i){
+                    arr[j] = liczby[i];
+                    j++;
+                }
+            }
+            liczby = arr;
+            rozmiar--;
+        }
+    }
+
+    public void usunPowtorzenia(){
+
     }
 }
--- src/pl/adamdylus/Main.java
+++ src/pl/adamdylus/Main.java
@@ -3,9 +3,22 @@ package pl.adamdylus;
 public class Main {
 
     public static void main(String[] args) {
-	    Lista lista = new Lista(10);
-        System.out.println(lista.getLiczby());
-        System.out.println(lista.getPojemnosc());
-        System.out.println(lista.getRozmiar());
+	    final int N = 10;
+        Lista l = new Lista(10);
+
+        for (int i = 0; i < N/2; ++i) {
+            l.dodajElement( (1 << i) );
+        }
+
+        l.dodajElement(2);
+        l.dodajElement(8);
+        l.pisz();
+        l.usunPierwszy(2);
+        l.pisz();
+
+        for (int i = 0; i < N/2; ++i) {
+            l.dodajElement( (1 << i) );
+        }
+        l.pisz();
     }
 }
